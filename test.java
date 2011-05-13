22004187b67fb562cbce61c56aeddbc7b2fb87cd
unknown <rchinoy@.mynewplace.local>
2011-05-12 22:50:33 -0700

    1st commit

---
+++ test.java
@@ -0,0 +1,12 @@
+ /**
+  * Created by IntelliJ IDEA.
+  * User: rchinoy
+  * Date: 5/12/11
+  * Time: 10:26 PM
+  * To change this template use File | Settings | File Templates.
+  */
+ public class test {
+     public static void main(String args[]){
+         System.out.println("Test 1st change");
+     }
+ }
